d81b58506344c159c2a48c868119e58c14ca8d09
LAB14_USER ON WS18 <L14X18W18@citulab.net>
2025-03-10 19:25:08 +0800

    Google Contacts Integration w/ fixes

---
+++ agravante.reisamson/contacts/contacts/src/main/java/com/agravante/contacts/config/PeopleServiceConfig.java
@@ -0,0 +1,22 @@
+package com.agravante.contacts.config;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.people.v1.PeopleService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+
+@Configuration
+public class PeopleServiceConfig {
+
+    public PeopleService peopleService(Credential credential) throws Exception {
+        return new PeopleService.Builder(
+                GoogleNetHttpTransport.newTrustedTransport(),
+                JacksonFactory.getDefaultInstance(),
+                credential
+        ).setApplicationName("FundNote").build();
+    }
+}
